74a694adeae51a10f0b47f97dad7137948bb6be8
Sandra <snelson3671@yahoo.com>
2018-08-14 10:16:28 -0500

    Add project files.

---
+++ CG 5-2 methodCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_5_2_methodCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
